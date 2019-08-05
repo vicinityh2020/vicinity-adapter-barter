00cedef5ba7064efdd089c50d71a1cf6c49d18fb
SashaP <sasha.pesic@vizlore.com>
2019-08-05 14:11:19 +0200

    changed contracts path

--- apps/apis/tasks.py
+++ apps/apis/tasks.py
@@ -26,7 +26,7 @@ def instantiate_dash_wallet(token, network_type, wallet_secret, chainrider_token
     payload = {
         "peers": DASH_PEERS,
         "chaincodeName": wallet_name,
-        "chaincodePath": "/home/chainrider-56c03/contracts/dash/",
+        "chaincodePath": "/home/chainrider-56c03/contracts_production/dash/",
         "chaincodeType": "node",
         "chaincodeVersion": "v0"
     }
@@ -127,7 +127,7 @@ def instantiate_bitcoin_wallet(token, network_type, wallet_secret, chainrider_to
     payload = {
         "peers": BITCOIN_PEERS,
         "chaincodeName": wallet_name,
-        "chaincodePath": "/home/chainrider-56c03/contracts/bitcoin/",
+        "chaincodePath": "/home/chainrider-56c03/contracts_production/bitcoin/",
         "chaincodeType": "node",
         "chaincodeVersion": "v0"
     }
@@ -226,7 +226,7 @@ def instantiate_data_storage(token, secret, oid, aid):
     payload = {
         "peers": REPOSITORY_PEERS,
         "chaincodeName": repo_name,
-        "chaincodePath": "/home/chainrider-56c03/contracts/data_storage/",
+        "chaincodePath": "/home/chainrider-56c03/contracts_production/data_storage/",
         "chaincodeType": "node",
         "chaincodeVersion": "v0"
     }
